525b332d77d94fe34a9ab6bc453c69bc373b9951
Alina Lebron <alebr002@fiu.edu>
2015-10-26 21:40:10 +0000

    Got application running + configured for testing routing engine. (created first routes)

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET /users/view */
+router.get('/view', function(req, res, next) {
+  res.send('user view');
+});
+
 module.exports = router;
